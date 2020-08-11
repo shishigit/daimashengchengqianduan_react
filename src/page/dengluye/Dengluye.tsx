c9f063bdeff3ac0ce73b1ba43c26a39b1ad90478
chendaliang <piaoyaocdl@163.com>
2020-08-11 16:45:29 +0800

    1

--- src/page/dengluye/Dengluye.tsx
+++ src/page/dengluye/Dengluye.tsx
@@ -1,7 +1,6 @@
-import {Avatar, Card} from "antd";
+import {Button, Card, Form, Input} from "antd";
 import React from "react";
 import './Dengluye.css'
-import {EditOutlined, EllipsisOutlined, SettingOutlined} from '@ant-design/icons';
 import Meta from "antd/es/card/Meta";
 
 interface shuxing
@@ -17,23 +16,29 @@ interface zhuangtai
 export class Dengluye extends React.Component<shuxing, zhuangtai>
 {
 
+    dengluxinxi = {
+        zhanghao: 'ceshi',
+        mima: 'ceshi'
+    }
+
     render(): React.ReactNode
     {
         return (
             <div className='beijing'>
                 <Card
                     style={{width: 300}}
-                    actions={[
-                        <SettingOutlined key="setting"/>,
-                        <EditOutlined key="edit"/>,
-                        <EllipsisOutlined key="ellipsis"/>,
-                    ]}
+                    actions={[<Button type="primary" style={{width: '95%'}}>登录</Button>]}
                 >
-                    <Meta
-                        avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"/>}
-                        title="Card title"
-                        description="This is the description"
-                    />
+                    <Meta title="测试" description="测试测试测试"/>
+
+                    <Form labelCol={{span: 4}} wrapperCol={{span: 20}} style={{marginTop: '50px'}}>
+                        <Form.Item label="帐号">
+                            <Input value={this.dengluxinxi.zhanghao}/>
+                        </Form.Item>
+                        <Form.Item label="密码">
+                            <Input.Password value={this.dengluxinxi.mima}/>
+                        </Form.Item>
+                    </Form>
                 </Card>
             </div>
         )

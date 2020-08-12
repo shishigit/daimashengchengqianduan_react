1ecb91f1b0d5a3d297cb62ab70aaa6824e27c7ec
chendaliang <piaoyaocdl@163.com>
2020-08-12 13:55:48 +0800

    1

--- src/page/zhuye/zhuye.tsx
+++ src/page/zhuye/zhuye.tsx
@@ -1,13 +1,62 @@
 import React from "react";
+import {Layout, Menu, Breadcrumb} from 'antd';
+
+import './zhuye.css'
+
+const {Header, Content, Footer, Sider} = Layout;
+const {SubMenu} = Menu;
 
 export class Zhuye extends React.Component<any, any>
 {
-    render(): React.ReactNode
+    state = {
+        collapsed: false,
+    };
+
+    onCollapse = (collapsed: any) =>
+    {
+        console.log(collapsed);
+        this.setState({collapsed});
+    };
+
+    render()
     {
         return (
-            <div>
-                fffffffffffffffffffff
-            </div>
-        )
+            <Layout style={{minHeight: '100vh'}}>
+                <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
+                    <div className="logo"/>
+                    <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+                        <Menu.Item key="1">
+                            Option 1
+                        </Menu.Item>
+                        <Menu.Item key="2">
+                            Option 2
+                        </Menu.Item>
+                        <SubMenu key="sub1" title="User">
+                            <Menu.Item key="3">Tom</Menu.Item>
+                            <Menu.Item key="4">Bill</Menu.Item>
+                            <Menu.Item key="5">Alex</Menu.Item>
+                        </SubMenu>
+                        <SubMenu key="sub2" title="Team">
+                            <Menu.Item key="6">Team 1</Menu.Item>
+                            <Menu.Item key="8">Team 2</Menu.Item>
+                        </SubMenu>
+                        <Menu.Item key="9"/>
+                    </Menu>
+                </Sider>
+                <Layout className="site-layout">
+                    <Header className="site-layout-background" style={{padding: 0}}/>
+                    <Content style={{margin: '0 16px'}}>
+                        <Breadcrumb style={{margin: '16px 0'}}>
+                            <Breadcrumb.Item>User</Breadcrumb.Item>
+                            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                        </Breadcrumb>
+                        <div className="site-layout-background" style={{padding: 24, minHeight: 360}}>
+                            Bill is a cat.
+                        </div>
+                    </Content>
+                    <Footer style={{textAlign: 'center'}}>Ant Design ©2018 Created by Ant UED</Footer>
+                </Layout>
+            </Layout>
+        );
     }
 }
